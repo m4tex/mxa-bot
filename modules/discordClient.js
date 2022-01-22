9175979e85928ebadf90777779a71525ef202b34
MatexPL <wikir88@gmail.com>
2022-01-22 16:25:40 +0100

    Cleaned the discordClient.js' code and added comments.

--- modules/discordClient.js
+++ modules/discordClient.js
@@ -1,34 +1,34 @@
-//Importing the file system module.
+//Importing needed modules.
 const fs = require('fs')
-
-//Importing config
 const config = require('../config.json')
-
 const Discord = require('discord.js')
-
-var discordBot = new Discord.Client(***REMOVED***
+//Creates the client
+let discordBot = new Discord.Client(***REMOVED***
   intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]
 ***REMOVED***);
 
+//adds a new field to the client that stores the commands.
 discordBot.commands = new Discord.Collection()
-var commandList = []
+//stores the list that is shown when using the 'help' command
+let commandList = []
 
-//I like how retarded it looks
+//I like how retarded it looks. This reads and imports the command modules from the /commands/ directory.
+// Then adds them to the commands' collection, the one assigned to the client.
 fs.readdirSync(config.localPath + 'commands/').filter(file => file.endsWith('.js'))
   .map(file => require(`$***REMOVED***config.localPath***REMOVED***commands/$***REMOVED***file***REMOVED***`)).forEach(command => ***REMOVED***
   discordBot.commands.set(command.name, command)
   commandList.push(command.name)
 ***REMOVED***);
 
-//I have to implement the command from here because it needs access to the commands and last attempts to do it went.. 
-//not so well.. so I just decided this is the cleanest way..
+//I have to implement the 'help' command from here because it needs access to the commands list and last attempts to implement it went...
+//not so well... so I just decided to do it this way.
 
-var helpCommand = ***REMOVED***
+let helpCommand = ***REMOVED***
   name: 'help',
-  description: 'Displays a full list of the commands, or a description of a single command. Usage: `mxa help command`',
+  description: 'Displays a full list of the commands, or a description of a single command. You can even specify a command after `\'help\'` to get information about the given command.',
   execute: function (msg, tokens) ***REMOVED***
     if(tokens[0] === undefined)***REMOVED***
-      var embed = new Discord.MessageEmbed().setColor('#0099ff').setTitle('Commands :sunglasses:')
+      let embed = new Discord.MessageEmbed().setColor('#0099ff').setTitle('Commands :sunglasses:')
         .setDescription(this.description)
         .addField("List of commands:", "`" + commandList.join(', ') + "`")
       msg.channel.send(***REMOVED***
@@ -36,9 +36,9 @@ var helpCommand = ***REMOVED***
       ***REMOVED***)
     ***REMOVED***
     else***REMOVED***
-      var cmd = discordBot.commands.get(tokens[0].toLowerCase())
+      let cmd = discordBot.commands.get(tokens[0].toLowerCase())
       if(cmd !== undefined)***REMOVED***
-        var embed = new Discord.MessageEmbed().setTitle('Command Help').addField('Command', '`' + tokens[0] + '`')
+        let embed = new Discord.MessageEmbed().setTitle('Command Help').addField('Command', '`' + tokens[0] + '`')
         .addField('Usage', '`' + cmd.usage + '`').addField('Description', cmd.description)
         msg.channel.send(***REMOVED***embeds: [embed]***REMOVED***)
       ***REMOVED***
@@ -48,9 +48,9 @@ var helpCommand = ***REMOVED***
     ***REMOVED***
   ***REMOVED***
 ***REMOVED***
-
+//adds the command to the commands, but it doesn't add the command to the list.
 discordBot.commands.set(helpCommand.name, helpCommand)
-
+//logs the bot into discord.
 discordBot.login(config.discordToken);
-
+//exports the client.
 module.exports = discordBot
