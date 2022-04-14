437218f6bbe9a13feaa18e75c7ded484bc3a40c3
MatexPL <wikir88@gmail.com>
2022-04-14 11:36:33 +0200

    hm

--- bot.js
+++ bot.js
@@ -3,15 +3,14 @@
 const config = require('./config.json')
 
 //MongoDB implementation (imports collections)
-let collections
-(async () => {
-    collections = await require('./modules/databaseManager.js')
-})()
+let collections = (async () => await require('./modules/databaseManager.js'))()
 
 //Discord module implementation
 const discordBot = require('./modules/discordClient')
 //Steam module implementation
 const steamClient = require('./modules/steamClient')
+
+"OTIwNDEzMDk3NjU3Nzg2NDQ5.Ybj_fg.iIYTo7APg_ihheLH3qR4hT8yy98"
 
 //#endregion
 
@@ -20,7 +19,7 @@ discordBot.on('messageCreate', async function (msg) {
     let prefix = 'mxa'
     //This checks for a custom prefix on a server.
     if (await collections.prefixes.countDocuments({serverID: msg.guildId}, {limit: 1})) {
-        prefix = (await collections.prefixes.findOne({serverID: msg.guildId})).bot_prefix
+        prefix = (await collections.prefixes.findOne({serverID: msg.guildId})).bot_prefixb
     }
     let tokens = msg.content.toLowerCase().split(/ +/)
     if (tokens.shift() === prefix) {
@@ -29,7 +28,7 @@ discordBot.on('messageCreate', async function (msg) {
         if (dcCommand !== undefined) {
             //This abomination takes care of permission restrictions for admin or dev commands. undefined = no permission requirements, 1 = for admins or above, 2 = for devs
             if (!dcCommand.hasOwnProperty('permLevel') || (dcCommand.permLevel === 1 && msg.member.permissions.has('ADMINISTRATOR') ||
-                (dcCommand.permLevel === 1 && msg.author.id === config.devDiscordId)|| (dcCommand.permLevel === 2 && msg.author.id === config.devDiscordId))) {
+                (dcCommand.permLevel === 1 && msg.author.id === config.devDiscordId) || (dcCommand.permLevel === 2 && msg.author.id === config.devDiscordId))) {
                 dcCommand.execute(msg, tokens)
             } else {
                 msg.channel.send('You don\'t have permissions to execute that command.');
